fe678b304885757a4cdf7bf9540290f2f90e2025
xbelda <xaviblx@gmail.com>
2020-11-27 15:59:31 +0100

    Added Seq2Seq model

--- src/model/nets.py
+++ src/model/nets.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 
+
 class Encoder(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_layers):
         super(Encoder, self).__init__()
@@ -35,6 +36,40 @@ class Decoder(nn.Module):
     def forward(self, x, h0):
         h0 = self.dropout(h0)
         output, hn = self.rnn(x, h0)
-        prediction = self.fc(output).flatten()
+        prediction = self.fc(output)
 
         return prediction, hn
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers):
+        super(Seq2Seq, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+
+        self.encoder = Encoder(input_dim, hidden_dim, num_layers)
+        self.decoder = Decoder(1, hidden_dim, num_layers)
+
+    def forward(self, x, y):
+        y_length = y.shape[0]
+        batch_size = y.shape[1]
+
+        _, encoder_hidden_out = self.encoder(x)
+
+        predictions = torch.zeros(y_length, batch_size).to(x.device)
+
+        # Take data from month -1 as first input for decoder
+        decoder_input = x[[-1]]
+        decoder_hidden = encoder_hidden_out
+
+        for i in range(y_length):
+            decoder_out, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+
+            predictions[i] = decoder_out.flatten()
+
+            # TODO: Add teacher forcing
+            decoder_input = decoder_out
+
+        return predictions.unsqueeze(dim=-1)
