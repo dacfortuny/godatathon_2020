afd4f75e625fda2fd0ddca16f3a73ceea2a3751b
xbelda <xaviblx@gmail.com>
2020-11-27 20:17:57 +0100

    Added pytorch-lightning class + Notebook

---
+++ src/model/trainer.py
@@ -0,0 +1,50 @@
+import pytorch_lightning as pl
+import torch
+from src.model.nets import Seq2Seq
+
+
+class RNNModel(pl.LightningModule):
+
+    def __init__(self, input_dim, hidden_dim, num_layers, lr=1e-2):
+        super().__init__()
+        self.lr = lr
+
+        self.save_hyperparameters()
+
+        self.model = Seq2Seq(input_dim, hidden_dim, num_layers)
+        self.loss_fc = torch.nn.MSELoss()
+
+    def forward(self, x, y):
+        return self.model(x, y)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+
+        x = x.float()
+        y = y.float()
+
+        x = x.permute(1, 0, 2)
+        y = y.permute(1, 0, 2)
+
+        y_hat = self(x, y)
+
+        loss = self.loss_fc(y_hat, y)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+
+        x = x.float()
+        y = y.float()
+
+        x = x.permute(1, 0, 2)
+        y = y.permute(1, 0, 2)
+
+        y_hat = self(x, y)
+        loss = self.loss_fc(y_hat, y)
+        self.log('val_loss', loss)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
