6a799ce1fa8ad87469ecd896abc668818fca8764
Didac Fortuny <dacfortuny@gmail.com>
2020-11-28 15:06:14 +0100

    cleaning

--- src/utils.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def read_raw_file(path, file):
-    return pd.read_csv(f"{path}/{file}", index_col=0)
-
-
-def save_dataset(df, path, file):
-    df.to_csv(f"{path}/{file}", index=False, encoding="utf-8")
