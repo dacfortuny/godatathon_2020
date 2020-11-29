360d01737d5bce5b4b81b50821fd56a1fb63ba43
xbelda <xaviblx@gmail.com>
2020-11-29 10:08:00 +0100

    Added modified loss to compute examples with few actual cases

--- src/metrics/loss.py
+++ src/metrics/loss.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import torch
+
+EPSILON = 1e-8
 
 
 def custom_novartis_metric(actuals, forecast, avg_volume):
@@ -94,28 +97,34 @@ def custom_metric(actuals, forecast, max_volume, avg_volume):
     actuals = actuals * max_volume / avg_volume
     forecast = forecast * max_volume / avg_volume
 
+    # Select only predictions with actual days
+    forecast = forecast[:len(actuals)]
+
     # Compute the first part of the equation
     # (custom MAPE with Average volume)
-    custom_mape = (actuals - forecast).abs().sum() / 24
+    n_elem = len(actuals)
+    custom_mape = (actuals - forecast).abs().sum() / (n_elem + EPSILON)
 
     # Compute the second part of the equation
     # (custom 6-first-months MAPE with Average volume)
-    six_month_mape = (actuals[:6].sum() - forecast[:6].sum()).abs() / 6
+    n_elem_first_6 = min(len(actuals), 6)
+    six_month_mape = (actuals[:6].sum() - forecast[:6].sum()).abs() / (n_elem_first_6 + EPSILON)
 
     # Compute the third part of the equation
     # (custom 6-months MAPE with Average volume)
-    twelve_month_mape = (actuals[6:12].sum() - forecast[6:12].sum()).abs() / 6
+    n_elem_second_6 = min(len(actuals) - 6, 6)
+    twelve_month_mape = (actuals[6:12].sum() - forecast[6:12].sum()).abs() / (n_elem_second_6 + EPSILON)
 
     # Compute the fourth part of the equation
     # (custom 12-months MAPE with Average volume)
-    last_month_mape = (actuals[12:].sum() - forecast[12:].sum()).abs() / 12
+    n_elem_over_12 = min(len(actuals) - 12, 12)
+    last_month_mape = (actuals[12:].sum() - forecast[12:].sum()).abs() / (n_elem_over_12 + EPSILON)
 
-    # Compute the custom metric
-    custom_metric = (0.5 * custom_mape
-                     + 0.3 * six_month_mape
-                     + 0.1 * (twelve_month_mape + last_month_mape))
+    loss = (0.5 * (n_elem / 24) * custom_mape
+            + 0.3 * (n_elem_first_6 / 6) * six_month_mape
+            + 0.1 * ((n_elem_second_6 / 6) * twelve_month_mape + (n_elem_over_12 / 12) * last_month_mape))
 
-    return custom_metric
+    return loss
 
 
 def uncertainty_metric(actuals, upper_bound, lower_bound, max_volume, avg_volume):
@@ -124,22 +133,35 @@ def uncertainty_metric(actuals, upper_bound, lower_bound, max_volume, avg_volume
     upper_bound = (upper_bound * max_volume / avg_volume)
     lower_bound = (lower_bound * max_volume / avg_volume)
 
-    uncertainty_first6 = (
-        # Wide intervals are penalized
-        0.85 * ((upper_bound[:6] - lower_bound[:6]).abs()).sum() +
-        0.15 * 2 / 0.05 * (
-            # If actuals are outside of the intervals, it adds error
-            ((lower_bound[:6] - actuals[:6]) * (actuals[:6] < lower_bound[:6])).sum() +
-            ((actuals[:6] - upper_bound[:6]) * (actuals[:6] > upper_bound[:6])).sum()
-        )
-     ) / 6
+    # Select only predictions with actual days
+    upper_bound = upper_bound[:len(actuals)]
+    lower_bound = lower_bound[:len(actuals)]
 
-    uncertainty_last18 = (
-        0.85 * ((upper_bound[6:] - lower_bound[6:]).abs()).sum()
-             + 0.15 * 2 / 0.05 * (
-                ((lower_bound[6:] - actuals[6:]) * (actuals[6:] < lower_bound[6:])).sum()
-                + ((actuals[6:] - upper_bound[6:]) * (actuals[6:] > upper_bound[6:])).sum()
-                                 )
-                         ) / 18
+    n_elem = len(actuals)
 
-    return 0.6 * uncertainty_first6 + 0.4 * uncertainty_last18
+    # Number of elements:
+    n_elem_lt_6 = min(n_elem, 6)
+    n_elem_mt_6 = n_elem - 6
+
+    # Up to 6
+    uncertainty_first = (
+                            # Wide intervals are penalized
+                                0.85 * ((upper_bound[:6] - lower_bound[:6]).abs()).sum() +
+                                0.15 * 2 / 0.05 * (
+                                    # If actuals are outside of the intervals, it adds error
+                                        ((lower_bound[:6] - actuals[:6]) * (actuals[:6] < lower_bound[:6])).sum() +
+                                        ((actuals[:6] - upper_bound[:6]) * (actuals[:6] > upper_bound[:6])).sum()
+                                )
+                        ) / (n_elem_lt_6 + EPSILON)
+
+    uncertainty_last = (
+                               0.85 * ((upper_bound[6:] - lower_bound[6:]).abs()).sum()
+                               + 0.15 * 2 / 0.05 * (
+                                       ((lower_bound[6:] - actuals[6:]) * (actuals[6:] < lower_bound[6:])).sum()
+                                       + ((actuals[6:] - upper_bound[6:]) * (actuals[6:] > upper_bound[6:])).sum()
+                               )
+                       ) / (n_elem_mt_6 + EPSILON)
+
+    loss = 0.6 * (n_elem_lt_6 / 6) * uncertainty_first + 0.4 * (n_elem_mt_6 / 18) * uncertainty_last
+
+    return loss
